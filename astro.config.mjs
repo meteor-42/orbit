756988eb03ff435678760f436b945199d4a66c80
Deface <m3t30r@proton.me>
2025-05-09 20:11:14 +0200

    009

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://meteor-42.xyz",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+  outDir: "server/build", // 👈 путь для сборки
 })
